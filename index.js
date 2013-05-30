7d35a7b1e13c91ff93074a4cd39a6f74f229e14e
Niall O'Higgins <niallo@unworkable.org>
2013-05-30 12:17:44 -0400

    add unshift example

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ var scriptPath = path.join(__filename, "script.sh")
 
 var commands = fs.readFileSync(scriptPath).split('\n')
 
+// commands.unshift("IP=201\n")
+// commands.unshift("SSID=myssid\n")
+
 module.exports = function(
 
 
